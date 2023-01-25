87a9e08b09f4ab520adbf0022f23073ca5e25f55
Immanuel01 <immanuel01@ui.ac.id>
2023-01-25 17:16:03 +0700

    refactor: move gradient definitions to style.js

---
+++ configuration/style.js
@@ -0,0 +1,58 @@
+
+/*
+Pendapat:
+Ridjky Tegar Perkasa:
+    - Paling bagus gradient4.
+    - Yang kedua paling bagus gradient1
+Adel:
+    - Gradient 1 lebih bagus daripada gradient 2
+OT:
+    - Gradient 1 lebih bagus daripada gradient 2
+Tantra:
+    - Gradient 2 lebih bagus daripada gradient 1, tapi gradient 1 juga cakep2 aja
+Tara:
+    - Gradient 1 lebih bagus daripada gradient 2
+Faiz:
+    - Gradient 4 lebih bagus daripada gradient 1
+    - Gradient 1 lebih bagus daripada gradient 2
+Bachtiar:
+    - Gradient 4 bagus
+    - Ranking keduanya gradient 1
+Idris:
+    - Gradient 1 paling bagus
+Zain:
+    - Gradient 4 bagus
+    - Gradient 2 dan 3 aneh
+    - Gradient 1 juga aneh
+ */
+const gradients = {
+    gradient1: [  // immanuel - 1.pdf
+        "#1C1F2F",
+        "#23273B",
+        "#282C42",
+        "#2C314A",
+        "#313652",
+    ],
+    gradient2: [  // immanuel - 2.pdf
+        "#192429",
+        "#1d292f",
+        "#212e36",
+        "#25343c",
+        "#293942",
+    ],
+    gradient3: [  // immanuel - 3.pdf
+        "#132020",
+        "#192929",
+        "#1f3233",
+        "#243b3d",
+        "#2a4546",
+    ],
+    gradient4: [  // Immanuel - 4.pdf
+        "#101323",
+        "#13162a",
+        "#161a31",
+        "#1a1e38",
+        "#1d213f",
+    ],
+}
+module.exports.gradients = gradients;
--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const {experienceGroups} = require("./data/ExperienceData");
 const {skillGroups} = require("./data/SkillData");
 const {myContacts} = require("./data/MyContactsData");
+const {gradients} = require("./configuration/style");
 
 const app = express();
 
@@ -11,40 +12,11 @@ app.use(express.static('public'));
 
 app.get("/", (req, res) => {
 
-    const gradient1 = [
-        "#2C4057",
-        "#1B2735",
-        "#2C4057",
-        "#1B2735",
-        "#2C4057",
-    ];
-    const gradient2 = [
-        "#1C1F2F",
-        "#263345",
-        "#1C1F2F",
-        "#263345",
-        "#1C1F2F",
-    ];
-    const gradient3 = [
-        "#1C1F2F",
-        "#23273B",
-        "#282C42",
-        "#2C314A",
-        "#313652",
-    ];
-    const gradient4 = [
-        "#192429",
-        "#1d292f",
-        "#212e36",
-        "#25343c",
-        "#293942",
-    ];
-
     res.render("portofolio.ejs", {
         "experienceGroups": experienceGroups,
         "skillGroups": skillGroups,
         "myContacts": myContacts,
-        "colorsForEachSection": gradient4,
+        "colorsForEachSection": gradients.gradient4,
     });
 });
 
