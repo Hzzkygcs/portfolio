d8fecfd77c56ddceca2ed282df22fb348ffe55e6
Immanuel01 <immanuel01@ui.ac.id>
2023-01-28 01:23:35 +0700

    feat(modal): improved modal UX
    Body element will not be scrollable if the modal is opened

--- public/modules/modal/modal.js
+++ public/modules/modal/modal.js
@@ -2,33 +2,55 @@ let numberOfModal = 0;
 
 const modalFadeAnimationDuration = 110;
 
-function showModal(title, elementsForContent=[], additionalClass=[]){
-    const templateElement = $("#main-modal-template");
-    const modal = $(templateElement.html());
 
-    for (const class_ of additionalClass) {
-        modal.addClass(class_);
-    }
+function showModal(title, contentDomElements=[], additionalClasses=[], modalParent=null){
+    modalParent = (modalParent == null)? $("body") : modalParent;
+    const modal = instantiateNewModalDomElement();
+    addClassesToModal(modal, additionalClasses);
 
-    const isClosed = new Deferred();
     const id = "modal-" + numberOfModal;
     modal.attr('id', id);
 
-
     modal.find(".modal-title").html(title);
-    const contentContainer = modal.find(".content");
-    for (const content of elementsForContent) {
-        contentContainer.append(content);
-    }
-    modal.find(".close-btn").on('click', () => {
-        isClosed.resolve();
-        modal.fadeOut(modalFadeAnimationDuration, () => modal.remove());
-    });
-
-    const body = $("body");
-    modal.hide().appendTo(body).fadeIn(modalFadeAnimationDuration);
+    appendContentsToModal(modal, contentDomElements)
+    const isClosed = new Deferred();
+    defineModalCloseEventHandler(modal, isClosed);
+    modal.hide().appendTo(modalParent).fadeIn(modalFadeAnimationDuration);
+    modalParent.addClass("on-modal-opened");
     return {
         isClosedPromise: isClosed,
         elementId: id,
     };
+}
+
+function instantiateNewModalDomElement() {
+    const templateElement = $("#main-modal-template");
+    return $(templateElement.html());
+}
+
+function addClassesToModal(modal, classes){
+    for (const $class of classes) {
+        modal.addClass($class);
+    }
+}
+
+
+function appendContentsToModal(modal, contentDomElements){
+    const contentContainer = modal.find(".content");
+    for (const content of contentDomElements) {
+        contentContainer.append(content);
+    }
+}
+
+
+/**
+ * @param modal
+ * @param {Deferred} isClosed
+ */
+function defineModalCloseEventHandler(modal, isClosed){
+    modal.find(".close-btn").on('click', () => {
+        isClosed.resolve();
+        $("body").removeClass("on-modal-opened");
+        modal.fadeOut(modalFadeAnimationDuration, () => modal.remove());
+    });
 }
