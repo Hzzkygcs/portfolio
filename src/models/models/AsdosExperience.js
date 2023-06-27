1cbfb76a66c0ba800f16988cbb6ee445da32e894
Immanuel01 <immanuel01@ui.ac.id>
2023-06-28 00:27:39 +0700

    refactor: Remove no-longer-used model-related files

--- src/models/models/AsdosExperience.js
+++
@@ -1,8 +0,0 @@
-export default class AsdosExperience{
-    constructor(startingMonthYear, endingMonthYear, courseName, courseTerm) {
-        this.startingMonthYear = startingMonthYear;
-        this.endingMonthYear = endingMonthYear;
-        this.courseName = courseName;
-        this.courseTerm = courseTerm;
-    }
-}
